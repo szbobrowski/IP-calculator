a7f4c9ed8455ccb9c3498baeb2fc002538ee04ed
Szymon Bobrowski <szymekbobrowski@wp.pl>
2021-03-19 00:09:00 +0100

    First commit. Source files. Start of the project.

---
+++ src/makaANDsimonovsky/com/Main.java
@@ -0,0 +1,8 @@
+package makaANDsimonovsky.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World of IP subnets!");
+    }
+}
---
+++ src/makaANDsimonovsky/com/MainMaka.java
@@ -0,0 +1,7 @@
+package makaANDsimonovsky.com;
+
+public class MainMaka {
+    public static void main(String[] args) {
+        System.out.println("Workspace for Marianna");
+    }
+}
