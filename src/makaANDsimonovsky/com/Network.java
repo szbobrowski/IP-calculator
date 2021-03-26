adb0ea55a106182773607095b3e7a07b166c746b
mariannakubsik <maka.kubsik1@gmail.com>
2021-03-26 14:03:19 +0100

    class Network and NetworkTest with firstHostAddress and lastHostAddress

--- src/makaANDsimonovsky/com/Network.java
+++ src/makaANDsimonovsky/com/Network.java
@@ -12,22 +12,92 @@ public class Network {
     protected double numberOfAvailableHosts;
     protected String broadcastAddressDecimal;
     protected String broadcastAddressBinary;
+    protected String firstHostAddress;
+    protected String lastHostAddress;
+
+    //Constructor
 
     public Network(String networkAddressDecimal, Integer networkMaskNumeral) {
         this.networkAddressDecimal = networkAddressDecimal;
         this.networkMaskNumeral = networkMaskNumeral;
         setNetworkAddressBinary();
-        convertMaskToBinary();
+        setNetworkMaskBinary();
+        setNetworkMaskDecimal();
+        setNumberOfAvailableHosts();
+        setBroadcastAddressBinary();
+        setBroadcastAddressDecimal();
+        setFirstHostAddress();
+        setLastHostAddress();
     }
 
-    //wgl co robimy czy podajemy adres sieci czy adres IP po prostu czy musi to byc adres sieci?
-    //co robimy z zabezpieczeniami w tej klasie czy w głównej? żeby nie było sytuacji gdzie będzie wieksze od 255 lub liczba ujemna
+    //Setters
 
     public void setNetworkAddressBinary() {
         this.networkAddressBinary = convertAddressToBinary(networkAddressDecimal);
     }
 
-    //zalezy nam na addressBinary w postaci 11111111.111100 itd. (znaczy z kropkami) czy moze tylko tak 1111000100101010 ...?
+    public void setNetworkMaskBinary() {
+        StringBuilder maskBinaryBuild = new StringBuilder();
+        maskBinaryBuild.append("1".repeat(Math.max(0, networkMaskNumeral)));
+        maskBinaryBuild.append("0".repeat(Math.max(0, 32 - networkMaskNumeral)));
+        maskBinaryBuild.insert(8, ".");
+        maskBinaryBuild.insert(17, ".");
+        maskBinaryBuild.insert(26, ".");
+        this.networkMaskBinary = maskBinaryBuild.toString();
+    }
+
+    public void setNetworkMaskDecimal() {
+        this.networkMaskDecimal = convertAddressToDecimal(networkAddressBinary);
+    }
+
+    public void setNumberOfAvailableHosts(){
+        int numberOfZeros = 32 - networkMaskNumeral;
+        this.numberOfAvailableHosts = pow(2,numberOfZeros) - 2;
+    }
+
+    public void setBroadcastAddressBinary(){
+        StringBuilder broadcastAddressBinaryBuild = new StringBuilder();
+        String networkAddressBinaryWithoutDots = networkAddressBinary.replace(".","");
+        String[] arrayOfAddress = networkAddressBinaryWithoutDots.split("");
+        String networkMaskBinaryWithoutDots = networkMaskBinary.replace(".","");
+        String[] arrayOfMask = networkMaskBinaryWithoutDots.split("");
+        for(int i = 0; i < 32; i++){
+            if(arrayOfMask[i].equals("1")){
+                broadcastAddressBinaryBuild.append(arrayOfAddress[i]);
+            }else{
+                broadcastAddressBinaryBuild.append("1");
+            }
+        }
+        broadcastAddressBinaryBuild.insert(8, ".");
+        broadcastAddressBinaryBuild.insert(17, ".");
+        broadcastAddressBinaryBuild.insert(26, ".");
+        this.broadcastAddressBinary = broadcastAddressBinaryBuild.toString();
+    }
+
+    public void setBroadcastAddressDecimal() {
+        this.broadcastAddressDecimal = convertAddressToDecimal(broadcastAddressBinary);
+    }
+
+    public void setFirstHostAddress() {
+        String[] arrayFirstHostAddress;
+        arrayFirstHostAddress = networkAddressDecimal.split("\\.");
+        int lastOctet = Integer.parseInt(arrayFirstHostAddress[3]);
+        lastOctet += 1;
+        arrayFirstHostAddress[3] = Integer.toString(lastOctet);
+        this.firstHostAddress = String.join(".", arrayFirstHostAddress);
+    }
+
+    public void setLastHostAddress() {
+        String[] arrayLastHostAddress;
+        arrayLastHostAddress = broadcastAddressDecimal.split("\\.");
+        int lastOctet = Integer.parseInt(arrayLastHostAddress[3]);
+        lastOctet -= 1;
+        arrayLastHostAddress[3] = Integer.toString(lastOctet);
+        this.lastHostAddress = String.join(".", arrayLastHostAddress);
+    }
+
+    //Methods
+
     protected String convertAddressToBinary(String addressDecimal){
         String[] arrayAddressDecimal;
         arrayAddressDecimal = addressDecimal.split("\\.");
@@ -55,13 +125,12 @@ public class Network {
         return addressBinary;
     }
 
-    public static String convertAddressToDecimal(String addressBinary){
+    public String convertAddressToDecimal(String addressBinary){
         String[] arrayAddressBinary;
         arrayAddressBinary = addressBinary.split("\\.");
         String addressDecimal;
         String [] arrayAddressDecimal = new String[4];
         for(int i = 0; i < 4; i++){
-            //int element = Integer.parseInt(arrayAddressDecimal[i]);
             String element = arrayAddressBinary[i];
             String[] arrayOfElement = element.split("");
             int newElement = 0;
@@ -70,51 +139,9 @@ public class Network {
                 int power = (int) pow(2,j);
                 newElement = newElement + argument*power;
             }
-            //arrayAddressBinary[i] = newElement.toString(); //czemu to nie dziala?
+            arrayAddressDecimal[i] = Integer.toString(newElement);
         }
-        return "addressDecimal";
-    }
-
-    //zrobic jako setter czy zostawic jak jest?
-    protected String convertMaskToBinary(){
-        StringBuilder maskBinaryBuild = new StringBuilder();
-        maskBinaryBuild.append("1".repeat(Math.max(0, networkMaskNumeral)));
-        maskBinaryBuild.append("0".repeat(Math.max(0, 32 - networkMaskNumeral)));
-        maskBinaryBuild.insert(8, ".");
-        maskBinaryBuild.insert(17, ".");
-        maskBinaryBuild.insert(26, ".");
-        this.networkMaskBinary = maskBinaryBuild.toString();
-        return networkMaskBinary;
-    }
-
-    protected double calculateNumberOfAvailableHosts(){
-        int numberOfZeros = 32 - networkMaskNumeral;
-        numberOfAvailableHosts = pow(2,numberOfZeros) - 2;
-        return numberOfAvailableHosts;
-    }
-
-    protected String findingBroadcastAddressBinary(){
-        StringBuilder broadcastAddressBinaryBuild = new StringBuilder();
-        String networkAddressBinaryWithoutDots = networkAddressBinary.replace(".","");
-        String[] arrayOfAddress = networkAddressBinaryWithoutDots.split("");
-        String networkMaskBinaryWithoutDots = networkMaskBinary.replace(".","");
-        String[] arrayOfMask = networkMaskBinaryWithoutDots.split("");
-        for(int i = 0; i < 32; i++){
-            if(arrayOfMask[i].equals("1")){
-                broadcastAddressBinaryBuild.append(arrayOfAddress[i]);
-            }else{
-                broadcastAddressBinaryBuild.append("1");
-            }
-        }
-        broadcastAddressBinaryBuild.insert(8, ".");
-        broadcastAddressBinaryBuild.insert(17, ".");
-        broadcastAddressBinaryBuild.insert(26, ".");
-        broadcastAddressBinary = broadcastAddressBinaryBuild.toString();
-        return broadcastAddressBinary;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World of IP subnets!");
-        convertAddressToDecimal("11000000.10101000.10000000.00000000");
+        addressDecimal = String.join(".", arrayAddressDecimal);
+        return addressDecimal;
     }
 }
--- src/makaANDsimonovsky/com/NetworkTest.java
+++ src/makaANDsimonovsky/com/NetworkTest.java
@@ -9,22 +9,36 @@ public class NetworkTest {
 
     @Test
     public void checkingConvertAddressToBinary() {
-        network.setNetworkAddressBinary();
         assertEquals("11000000.10101000.10000000.00000000", network.networkAddressBinary);
     }
 
     @Test
+    public void checkingConvertAddressToDecimal() {
+        assertEquals("192.168.191.255", network.broadcastAddressDecimal);
+    }
+
+    @Test
     public void checkingConvertMaskToDecimal() {
-        assertEquals("11111111.11111111.11000000.00000000", network.convertMaskToBinary());
+        assertEquals("11111111.11111111.11000000.00000000", network.networkMaskBinary);
     }
 
     @Test
     public void checkingCalculateNumberOfAvailableHosts() {
-        assertEquals(16382, network.calculateNumberOfAvailableHosts());
+        assertEquals(16382, network.numberOfAvailableHosts);
     }
 
     @Test
     public void checkingFindingBroadcastAddressBinary() {
-        assertEquals("11000000.10101000.10111111.11111111", network.findingBroadcastAddressBinary());
+        assertEquals("11000000.10101000.10111111.11111111", network.broadcastAddressBinary);
+    }
+
+    @Test
+    public void checkingFindingFirstHostAddressDecimal() {
+        assertEquals("192.168.128.1", network.firstHostAddress);
+    }
+
+    @Test
+    public void checkingFindingLastHostAddressDecimal() {
+        assertEquals("192.168.191.254", network.lastHostAddress);
     }
 }
