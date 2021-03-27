ff621056a3dbbb4db2893edb3b626f888a647eaa
Szymon Bobrowski <szymekbobrowski@wp.pl>
2021-03-27 21:57:37 +0100

    one import deleted in Network class

--- src/makaANDsimonovsky/com/NetworkTest.java
+++ src/makaANDsimonovsky/com/NetworkTest.java
@@ -1,7 +1,7 @@
 package makaANDsimonovsky.com;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NetworkTest {
 
